d36c8cb78acf427d36bfc5f24d9b3d8782fe399f
Yuu-Miino <miino.yuu@gmail.com>
2023-03-16 01:52:53 +0900

    continued

---
+++ crop_tools/__main__.py
@@ -0,0 +1,3 @@
+from crop_tools import crop_face_dir
+
+crop_face_dir(verbose=True, width=250, height=250)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="crop_tools",
+    version="0.0.1",
+    author="Yuu-Miino-NUE",
+    description="Tools to crop by OpenCV",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.10',
+)
