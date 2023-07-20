5151262c4471d68a7ef49c3b4fe80b19f3321727
Yuu-Miino-NUE <ymiino@naruto-u.ac.jp>
2023-07-21 00:00:57 +0900

    Minor update

--- sample_data/sample.py
+++ sample_data/sample.py
@@ -1,9 +1,30 @@
-from crop_tools import crop_face, crop_face_dir
+from crop_tools import crop_face_to_file, crop_face_dir
 
 # For a single file
-crop_face(input='input/test1.jpg', output='output/htu_test1.jpg', width=250, height=250, margin = 1.8, frame = True, classifier='alt',
-    minSize=(50, 50), maxSize=(100, 100), scaleFactor=1.1, minNeighbors=4)
+crop_face_to_file(
+    input="input/test1.jpg",
+    output="output/htu_test1.jpg",
+    width=250,
+    height=250,
+    margin=1.8,
+    frame=True,
+    classifier="alt",
+    minSize=(50, 50),
+    maxSize=(100, 100),
+    scaleFactor=1.1,
+    minNeighbors=4,
+)
 
 # For all files in a directory
-crop_face_dir(input_dir='input', output_dir='output', verbose=True, width=200, height=200, margin=1.8, frame=True, classifier='default',
-    maxSize=(100,100), scaleFactor=1.2)
+crop_face_dir(
+    input_dir="input",
+    output_dir="output",
+    verbose=True,
+    width=200,
+    height=200,
+    margin=1.8,
+    frame=True,
+    classifier="default",
+    maxSize=(100, 100),
+    scaleFactor=1.2,
+)
--- src/crop_tools/__init__.py
+++ src/crop_tools/__init__.py
@@ -13,6 +13,7 @@ from cv2 import (
     CascadeClassifier,
     COLOR_BGR2GRAY,
     rectangle,
+    Mat,
 )
 from cv2.data import haarcascades
 from numpy import array, maximum, minimum
@@ -32,8 +33,7 @@ class ExtentionError(Exception):
 
 
 def crop_face(
-    input: str,
-    output: str | None = None,
+    input: str | Mat,
     width: int = 300,
     height: int = 300,
     margin: float = 1.75,
@@ -48,7 +48,132 @@ def crop_face(
     maxSize: tuple[int, int] = (100, 100),
     scaleFactor: float = 1.1,
     minNeighbors: int = 4,
-):
+) -> Mat | None:
+    """Generate face cropped jpg file
+
+    Parameters
+    ----------
+    input : str
+        Input filename.
+    width : int, optional
+        Width of the output jpg, by default ``300``.
+    height : int, optional
+        Height of the output jpg, by default ``300``.
+    margin : float, optional
+        Ratio of margin to the detected face, by default ``1.75``.
+    show : bool, optional
+        Flag to immediately show the output image, by default ``False``.
+    frame : bool, optional
+        Flag to show the frame of the detected face, by default ``False``.
+    classifier : Literal[ 'default', 'alt', 'alt2', ], optional
+        Cascade classifier, by default ``default``.
+    minSize : Tuple[int, int], optional
+        Minimum possible object size, by default ``(50, 50)``.
+    maxSize : Tuple[int, int], optional
+        Maximum possible object size, by default ``(100, 100)``.
+    scaleFactor : float, optional
+        Parameter specifying how much the image size is reduced at each image scale, by default ``1.1``.
+    minNeihbors : int
+        Parameter specifying how many neighbors each candidate rectangle should have to retain it, by default ``4``.
+
+    Return
+    ------
+    output : numpy.ndarray
+        Output binary
+
+    Raises
+    ------
+    ExtentionError
+        Imcompatible extensions specified.
+    FileNotFoundError
+        Input file not exist.
+    """
+    if isinstance(input, str):
+        if not input.endswith(EXTENSIONS):
+            raise ExtentionError
+        else:
+            if not os.path.isfile(input):
+                raise FileNotFoundError(f"No such file or directory: '{input}'")
+        img = imread(input)
+    else:
+        img = input
+
+    gray = cvtColor(img, COLOR_BGR2GRAY)
+    aspect_ratio = height / width
+
+    cascade_path = os.path.join(
+        haarcascades, "haarcascade_frontalface_" + classifier + ".xml"
+    )
+
+    face_cascade = CascadeClassifier(cascade_path)
+    faces = face_cascade.detectMultiScale(
+        image=gray,
+        scaleFactor=scaleFactor,
+        minNeighbors=minNeighbors,
+        minSize=minSize,
+        maxSize=maxSize,
+    )
+
+    face = None
+    for x, y, w, h in faces:
+        center = array([y + h / 2, x + w / 2], dtype=int)
+        if frame:
+            rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+
+        im_h, im_w, _ = img.shape
+        if w * aspect_ratio > h:
+            half_diag = array(margin * array([w / 2 * aspect_ratio, w / 2]), dtype=int)
+            ymin, xmin = maximum(center - half_diag, array([0, 0]), dtype=int)
+            ymax, xmax = minimum(center + half_diag, array([im_h, im_w]), dtype=int)
+        else:
+            half_diag = array(margin * array([h / 2, h / 2 / aspect_ratio]), dtype=int)
+            ymin, xmin = maximum(center - half_diag, array([0, 0]), dtype=int)
+            ymax, xmax = minimum(center + half_diag, array([im_h, im_w]), dtype=int)
+
+        if ymin == 0 or ymax == im_h:
+            xmax = int(center[1] + (ymax - ymin) / aspect_ratio / 2)
+            xmin = int(center[1] - (ymax - ymin) / aspect_ratio / 2)
+            print(
+                "[Warning] underfull width due to the image height, margin ignored. ",
+                end="",
+            )
+        elif xmin == 0 or xmax == im_w:
+            ymax = int(center[0] + (xmax - xmin) * aspect_ratio / 2)
+            ymin = int(center[0] - (xmax - xmin) * aspect_ratio / 2)
+            print(
+                "[Warning] underfull height due to the image width, margin ignored. ",
+                end="",
+            )
+        else:
+            pass
+
+        face = resize(img[ymin : ymax + 1, xmin : xmax + 1], (width, height))
+        if show:
+            imshow("Type any key to quit", face)
+
+    if show:
+        waitKey()
+    return face
+
+
+def crop_face_to_file(
+    input: str | Mat,
+    output: str,
+    width: int = 300,
+    height: int = 300,
+    margin: float = 1.75,
+    show: bool = False,
+    frame: bool = False,
+    classifier: Literal[
+        "default",
+        "alt",
+        "alt2",
+    ] = "default",
+    minSize: tuple[int, int] = (50, 50),
+    maxSize: tuple[int, int] = (100, 100),
+    scaleFactor: float = 1.1,
+    minNeighbors: int = 4,
+) -> bool:
     """Generate face cropped jpg file
 
     Parameters
@@ -85,76 +210,25 @@ def crop_face(
     FileNotFoundError
         Input file not exist.
     """
-    if not input.endswith(EXTENSIONS):
-        raise ExtentionError
-    else:
-        if not os.path.isfile(input):
-            raise FileNotFoundError(f"No such file or directory: '{input}'")
+    if not output.endswith(EXTENSIONS):
+        output += ".jpg"
 
-    if output is None:
-        output = "".join(input.split(".")[:-1]) + "-out.jpg"
-    else:
-        if not output.endswith(EXTENSIONS):
-            output += ".jpg"
-
-    img = imread(input)
-    gray = cvtColor(img, COLOR_BGR2GRAY)
-    aspect_ratio = height / width
-
-    cascade_path = os.path.join(
-        haarcascades, "haarcascade_frontalface_" + classifier + ".xml"
-    )
-
-    face_cascade = CascadeClassifier(cascade_path)
-    faces = face_cascade.detectMultiScale(
-        image=gray,
-        scaleFactor=scaleFactor,
-        minNeighbors=minNeighbors,
+    face = crop_face(
+        input=input,
+        width=width,
+        height=height,
+        margin=margin,
+        show=show,
+        frame=frame,
+        classifier=classifier,
         minSize=minSize,
         maxSize=maxSize,
+        scaleFactor=scaleFactor,
+        minNeighbors=minNeighbors,
     )
-
-    result = False
-    for x, y, w, h in faces:
-        center = array([y + h / 2, x + w / 2], dtype=int)
-        if frame:
-            rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
-
-        im_h, im_w, _ = img.shape
-        if w * aspect_ratio > h:
-            half_diag = array(margin * array([w / 2 * aspect_ratio, w / 2]), dtype=int)
-            ymin, xmin = maximum(center - half_diag, array([0, 0]), dtype=int)
-            ymax, xmax = minimum(center + half_diag, array([im_h, im_w]), dtype=int)
-        else:
-            half_diag = array(margin * array([h / 2, h / 2 / aspect_ratio]), dtype=int)
-            ymin, xmin = maximum(center - half_diag, array([0, 0]), dtype=int)
-            ymax, xmax = minimum(center + half_diag, array([im_h, im_w]), dtype=int)
-
-        if ymin == 0 or ymax == im_h:
-            xmax = int(center[1] + (ymax - ymin) / aspect_ratio / 2)
-            xmin = int(center[1] - (ymax - ymin) / aspect_ratio / 2)
-            print(
-                "[Warning] underfull width due to the image height, margin ignored. ",
-                end="",
-            )
-        elif xmin == 0 or xmax == im_w:
-            ymax = int(center[0] + (xmax - xmin) * aspect_ratio / 2)
-            ymin = int(center[0] - (xmax - xmin) * aspect_ratio / 2)
-            print(
-                "[Warning] underfull height due to the image width, margin ignored. ",
-                end="",
-            )
-        else:
-            pass
-
-        face = resize(img[ymin : ymax + 1, xmin : xmax + 1], (width, height))
-        if show:
-            imshow("Type any key to quit", face)
-        result = imwrite(output, face)
-
-    if show:
-        waitKey()
-    return result
+    if face is None:
+        return False
+    return imwrite(output, face)
 
 
 def crop_face_dir(
@@ -188,6 +262,6 @@ def crop_face_dir(
     for jpg in jpg_list:
         if verbose:
             print(f"Processing `{jpg}`... ", end="")
-        crop_face(input_dir + "/" + jpg, output_dir + "/" + jpg, **options)
+        crop_face_to_file(input_dir + "/" + jpg, output_dir + "/" + jpg, **options)
         if verbose:
             print("Done")
